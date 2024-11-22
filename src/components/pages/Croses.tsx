d5001781211d1e47f1ff9152a34d386b2322ccc4
DenysDominskyi <denysdominskyi@gmail.com>
2024-11-22 15:01:27 +0100

    Sprint04-02

---
+++ src/components/pages/Croses.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+export const Croses = () => {
+  return (
+    <div>
+      <div>HEADER</div>
+      <Outlet />
+      <div>FOOTER</div>
+    </div>
+  );
+};
+//----------------------
+
+// import { lazy, Suspense } from "react";
+
+// const Adidas = lazy(() => import("./Adidas"));
+// const Puma = lazy(() => import("./Puma"));
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "adidas",
+//         element: (
+//           <Suspense fallback={<div>Loading...</div>}>
+//             <Adidas />
+//           </Suspense>
+//         ),
+//       },
+//       {
+//         path: "puma",
+//         element: (
+//           <Suspense fallback={<div>Loading...</div>}>
+//             <Puma />
+//           </Suspense>
+//         ),
+//       },
+//     ],
+//   },
+// ]);
+
+// useNavigation, useMatches, useRouterError
